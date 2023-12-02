f9698d90ee4e23ed63eab97ba7f14ebef8c4584a
Roman Oboroceanu <r.oboroceanu@gmail.com>
2023-12-02 16:48:18 +0000

    good/feat

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import {useState} from 'react';
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
 function App() {
- const [cartIsShown, setCartIsShown] =  useState(false);
- function showCartHandler(){
-  setCartIsShown(true);
- }
- function hideCartHandler(){
-  setCartIsShown(false);
- }
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <CartProvider>
-      {cartIsShown && <div>Cart...</div>}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
